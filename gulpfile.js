2678b3723ad4dc932e35de28e47e8c9d304570a3
wuyang910217 <wuyang910217@gmail.com>
2016-09-09 15:51:51 +0800

    add gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     prefix = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
     wrap = require('gulp-wrap'),
     browserSync = require('browser-sync'),
     imagemin = require('gulp-imagemin'),
@@ -42,6 +43,7 @@ gulp.task('sass', function(){
   gulp.src('./source/styles/main.scss')
           .pipe(sass()).on('error', handleError)
           .pipe(prefix())
+          .pipe(cleanCSS({compatibility: 'ie8'}))
           .pipe(gulp.dest('./dist/styles/'))
           .pipe(browserSync.reload({stream: true}));
 });
